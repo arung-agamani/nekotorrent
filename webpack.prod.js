908e7ede8d21f9cfa5e136137a18cadd8349ca0b
arung-agamani <arung.agamani@gmail.com>
2021-01-31 16:12:02 +0800

    Added production webpack config and build script

---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+    template: './web/src/index.html'
+});
+
+const config = {
+    mode: 'production',
+    entry: './web/src/index.tsx',
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                test: /\.(jpe?g|gif|png|svg)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [htmlPlugin]
+}
+
+module.exports = config
